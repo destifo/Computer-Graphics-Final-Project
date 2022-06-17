38dff581e7bccfd9c73395fc2db71febe46bb6c8
SimonMekonnen <simonmekonnen80@gmail.com>
2022-06-17 11:10:31 -0800

    defining the pipes and the bird

--- main.py
+++ main.py
@@ -1,10 +1,172 @@
+from operator import index
+import pipes
+from pprint import pp
+import random as random
+from sys import flags
+from turtle import pos
 import pygame
-import random
-from PIL import Image
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from pygame.locals import *
+from OpenGL.GL.shaders import *
 import numpy as np
+import os
+from PIL import Image
+class Start:
+    def __init__(self,vertex,fragment):
+        
+        pygame.init()
+
+        pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 4)
+        pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 1)
+        pygame.display.gl_set_attribute(
+        pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
+
+        display = (500, 500)
+        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+        
+        glClearColor(0.30, 0.20, 0.20, 1.0)
+        glViewport(0, 0, 500, 500)
+
+        vertex_shader_content = self.getFileContents(vertex)
+        fragment_shader_content = self.getFileContents(fragment)
+
+        vertex_shader = compileShader(vertex_shader_content, GL_VERTEX_SHADER)
+        fragment_shader = compileShader(
+            fragment_shader_content, GL_FRAGMENT_SHADER)
+
+        self.program = glCreateProgram()
+        glAttachShader(self.program, vertex_shader)
+        glAttachShader(self.program, fragment_shader)
+        glLinkProgram(self.program)
+       
+        glUseProgram(self.program)
+        
+    def getFileContents(self,filename):
+        p = os.path.join(os.getcwd(), "shaders", filename)
+        return open(p, 'r').read()
+class Bird:
+     def __init__(self):
+        start = Start(vertex="bg.vertex.shader",fragment="bg.fragment.shader")
+        self.birdVertexes = np.array([
+        # position          # color           # texture s, r
+        [-0.6, 0.1, 0,    1.0, 0.20, 0.8, 1.0, 1.0],
+        [-0.6, -0.1, 0,   1.0, 1.0, 0.0, 1.0, 0.0],
+        [-0.8, 0.1, 0,   0.0, 0.7, 0.2, 0.0, 1.0],
+
+        [-0.6, -0.1, 0,   1.0, 1.0, 0.0, 1.0, 0.0],
+        [-0.8, -0.1, 0,  0.0, 0.4, 1.0, 0.0, 0.0],
+        [-0.8, 0.1, 0,   0.0, 0.7, 0.2, 0.0, 1.0],
+
+    ], dtype=np.float32)
+
+        self.birdVao = glGenVertexArrays(1)
+        self.birdVbo = glGenBuffers(1)
+        glBindVertexArray(self.birdVao)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.birdVbo)
+
+        glBufferData(GL_ARRAY_BUFFER, self.birdVertexes.nbytes, self.birdVertexes, GL_STATIC_DRAW)
+        positionLocation = glGetAttribLocation(start.program, "position")
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                            8 * self.birdVertexes.itemsize, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(0)
+
+        colorLocation = glGetAttribLocation(start.program, "color")
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
+                            8 * self.birdVertexes.itemsize, ctypes.c_void_p(12))
+        glEnableVertexAttribArray(1)
+
+        textureLocation = glGetAttribLocation(start.program, "texCoord")
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE,
+                            8 * self.birdVertexes.itemsize, ctypes.c_void_p(24))
+        glEnableVertexAttribArray(2)
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+ 
+        glBindVertexArray(0)
+        self.tex = Texture("textures/bird3.png")
+     def update(self,x=0,y=0): 
+        self.birdVertexes[0][0]+=x
+        self.birdVertexes[1][0]+=x
+        self.birdVertexes[2][0]+=x
+        self.birdVertexes[3][0]+=x
+        self.birdVertexes[4][0]+=x
+        self.birdVertexes[5][0]+=x
+        self.birdVertexes[0][1]+=y      
+        self.birdVertexes[1][1]+=y       
+        self.birdVertexes[2][1]+=y       
+        self.birdVertexes[3][1]+=y   
+        self.birdVertexes[4][1]+=y   
+        self.birdVertexes[5][1]+=y   
+        glBindBuffer(GL_ARRAY_BUFFER, self.birdVbo)   
+        glBufferData(GL_ARRAY_BUFFER, self.birdVertexes.nbytes, self.birdVertexes, GL_STATIC_DRAW)
+       
+class Pipe:
+    def __init__(self,translate=(0,0)):
+        start = Start(vertex="bg.vertex.shader",fragment="bg.fragment.shader")
+        self.translate = translate
+        self.pipeVertex = np.array([
+        [1.0, 0.2, 0.0, 0.0, 1.0, 0.0,0,0],
+        [1.0, 2.0, 0.0, 0.0, 1.0,0.0,0,1],
+        [1.2, 2.0, 0.0, 0.0, 1.0, 0.0,1,1],
+        [1.2, 0.2, 0.0, 0.0, 1.0, 0.0,1,0],], dtype=np.float32)
+       
+        self.pipeVertex =  np.array([
+        [1.0+translate[0], 0.2+translate[1], 0.3, 0.0, 1.0, 0.5,0.0,0.0],
+        [1.0+translate[0], 2.0+translate[1], 0.3, 0.0, 1.0,0.5,0.0,1.0    ],
+        [1.2+translate[0], 2.0+translate[1], 0.3, 0.0, 1.0, 0.5,1.0,1.0 ],
+        [1.2+translate[0], 0.2+translate[1], 0.3, 0.0, 1.0, 0.5,1.0,0.0],], dtype=np.float32)
+        self.pipeIndices=np.array([0,1,2,
+                       0,3,2 ],dtype=np.int32)
+        self.pipeVAO = glGenVertexArrays(1)
+
+        glBindVertexArray(self.pipeVAO)
+    
+        self.pipeVBO = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.pipeVBO)
+        glBufferData(GL_ARRAY_BUFFER, self.pipeVertex.nbytes, self.pipeVertex, GL_STATIC_DRAW)
+   
+
+        self.indexVBO = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.indexVBO)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.pipeIndices.nbytes, self.pipeIndices, GL_STATIC_DRAW)
+    
+        positionLocation = glGetAttribLocation(start.program, "position")
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                            8 * self.pipeVertex.itemsize, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(0)
+ 
+        colorLocation = glGetAttribLocation(start.program, "color")
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
+                            8 * self.pipeVertex.itemsize, ctypes.c_void_p(3 * 4))
+        glEnableVertexAttribArray(1)
+        textureLocation = glGetAttribLocation(start.program, "texCoord")
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE,
+                            8 * self.pipeVertex.itemsize, ctypes.c_void_p(24))
+        glEnableVertexAttribArray(2)
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+ 
+        glBindVertexArray(0)
+        self.tex = Texture("textures/pipe.jpg")
+        
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindVertexArray(0)
+        
+    def update(self,x=0,y=0):
+        self.pipeVertex[0][0]+=x
+        self.pipeVertex[1][0]+=x
+        self.pipeVertex[2][0]+=x
+        self.pipeVertex[3][0]+=x
+        self.pipeVertex[0][1]+=y      
+        self.pipeVertex[1][1]+=y       
+        self.pipeVertex[2][1]+=y       
+        self.pipeVertex[3][1]+=y   
+        glBindBuffer(GL_ARRAY_BUFFER, self.pipeVBO)   
+        glBufferData(GL_ARRAY_BUFFER, self.pipeVertex.nbytes, self.pipeVertex, GL_STATIC_DRAW)
+
+
 
 
 class Backgroundimage:
