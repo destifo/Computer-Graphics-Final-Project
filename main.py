389a5ab4a5b6a9e60a2330707008949277d76201
destifo <estifanosbireda@gmail.com>
2022-06-17 12:15:48 +0300

    deleted old unnecessary files and added collision check logic

--- main.py
+++ main.py
@@ -230,104 +230,36 @@ class Texture:
         glGenerateMipmap(GL_TEXTURE_2D)
 
 
+prev_block_pos = 0
+y=round(random.uniform(-0.60, 0.60), 2)
+pipe = Pipe(translate=(-0.5,0.05+y))
+pipe2= Pipe(translate=(-0.5,-2.25+y))
+pipes = [pipe,pipe2]
 
-
-
-
-
-
-
-
-
-
-
-def init():
-    pygame.init()
-    display = (500, 750)
-    display = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluOrtho2D(0, 500,750,0)
-    return display
-
-
-game_over = False
-bg_img = pygame.image.load('background.jpg')
-# bird_img = pygame.image.load('bird1.png')
-# raw_data = bird_img.get_buffer().raw
-# data = np.frombuffer(raw_data, np.uint8)
-
-display = init()
-clock = pygame.time.Clock()
-
-#Bird Object
-def draw_bird(x, y, w, h, img):
-    # raw_data = img.get_buffer().raw
-    # data = np.fromstring(raw_data, np.uint8)
-    Texture = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, Texture)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB,GL_UNSIGNED_BYTE, img_data)
-    glEnable(GL_TEXTURE_2D)
-
-    glEnable(GL_TEXTURE_2D)
-    glBindTexture(GL_TEXTURE_2D, Texture)
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    glTranslate(x, y, 0)
-
-    glBegin(GL_QUADS)
-    glTexCoord2f(0, 0)
-    glVertex(0, 0, 0)
-
-    glTexCoord2f(0, 1)
-    glVertex(w, 0, 0)
-
-    glTexCoord2f(1, 1)
-    glVertex(w, h, 0)
-
-    glTexCoord2f(1, 0)
-    glVertex(0, h, 0)
-    glEnd()
-    glDisable(GL_TEXTURE_2D)
-    glFlush()
-
-
-bird_x = 50
-bird_y = 300
-yint_change = 0
-img = Image.open('bird1.png')
-img_data = img.tobytes("raw", "RGB", 0, -1)
-
-
-while not game_over:
-    clock.tick(60)
-    display.fill((0, 0, 0))
-
-    # display2 = pygame.display.set_mode((500, 750))
-
-    display.blit(bg_img, (0, 0))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                yint_change = -5
+def check_collision(bird_pos, x, blocks_y_pos):
+    left_x = x[0]
+    right_x = x[1]
+    upper_y_limit = blocks_y_pos[0]
+    lower_y_limit = blocks_y_pos[1]
+    bird_left_x = bird_pos[0][0]
+    bird_right_x = bird_pos[0][1]
+    bird_up_y = bird_pos[1][0]
+    bird_down_y = bird_pos[1][1]
+    print(bird_right_x)
+    print(bird_left_x, left_x, right_x)
+    if (bird_right_x >= left_x and bird_right_x <= right_x) or (bird_left_x >= left_x and bird_left_x <= right_x):
+        print(bird_up_y, upper_y_limit)
+        print(bird_down_y, lower_y_limit)
+        if (bird_down_y <= lower_y_limit):
+            print('lower check works')
+            return True
+        if (bird_up_y >= upper_y_limit):
+            print('upper check works')
+            return True
         
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                yint_change = 3
-
-    bird_y += yint_change
-    draw_bird(bird_x, bird_y, img.size[0], img.size[1], img_data)
-
-    pygame.display.flip()
+    return False
+    
+pipe_len = 2
 
 
-pygame.quit()
+
